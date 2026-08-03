9bfb010d5914cfcd2f3d9980faa8fe1d943bbbe9
casper-justus <njahjustus@gmail.com>
2026-08-03 11:34:15 +0300

    Fix: Migrate Android build to modern Flutter Gradle plugin
    The legacy 'apply from: flutter.gradle' skips adding the Flutter embedding
    when plugins exist, so io.flutter.* was unresolvable in MainActivity.kt.
    Migrate to declarative plugins block:
    - settings.gradle: flutter-plugin-loader with pluginManagement includeBuild
    - build.gradle: AGP 8.3.0 + Kotlin 1.9.20 apply false
    - app/build.gradle: dev.flutter.flutter-gradle-plugin, flutter.versionCode()/
      flutter.versionName() (build-name/build-number flags still honored)
    - MainActivity.kt: drop manual GeneratedPluginRegistrant registration

--- android/app/src/main/java/com/ytmusic/client/MainActivity.kt
+++ android/app/src/main/java/com/ytmusic/client/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "com.ytmusic.client/ota"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        super.configureFlutterEngine(flutterEngine)
 
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             when (call.method) {
